acda1942e1f9a583502b4b5674f6057ef1cdfea2
Rob Hofmann <rob.hofmann@outlook.com>
2021-04-18 17:43:24 +0200

    fix voor string values in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [[ -n "$CLEAN_CRON_EXPRESSION" ]]; then
   ln -sf /proc/$$/fd/1 /var/log/stdout
   service cron start
 	if [[ -n "$CLEAN_CRON_EXPRESSION" ]]; then
-        echo "$CLEAN_CRON_EXPRESSION pwsh /scripts/Purge.ps1 -RootDirectory '/data' -GracePeriodInSeconds $GRACE_PERIOD_IN_SECONDS -IncludeFilters "$INCLUDE_FILTERS" -ExcludeFilters "$EXCLUDE_FILTERS" >/var/log/stdout 2>&1" > /etc/crontab
+        echo "$CLEAN_CRON_EXPRESSION pwsh /scripts/Purge.ps1 -RootDirectory '/data' -GracePeriodInSeconds $GRACE_PERIOD_IN_SECONDS -IncludeFilters '"$INCLUDE_FILTERS"' -ExcludeFilters '"$EXCLUDE_FILTERS"' >/var/log/stdout 2>&1" > /etc/crontab
 	fi
 	crontab /etc/crontab
 fi
